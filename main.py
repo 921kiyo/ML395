858c8514974b007bc23752217167a45afd976867
Kiyohito Kunii <kk3317@voxel01.doc.ic.ac.uk>
2018-01-25 16:42:22 +0000

    finish until getting largest info gain

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 
 import scipy.io as sio
@@ -33,24 +34,38 @@ print(sio.whosmat('Data/cleandata_students.mat'))
 # In training function
 
 def transform_emotion(vector, emotion):
-    for v in vector:
-        if v != emotion:
-            v = 0
-            # print(v)
+    for v in range(len(vector)):
+        if vector[v] != emotion:
+            vector[v] = 0
         else:
-            v = 1
-            # print(v)
+            vector[v] = 1
+    return vector
+
 transform_emotion(mat_y, 4)
+print(mat_y)
 
 class Tree:
-    def __init__(self):
-        self.op = decision_tree_learning(mat_x, mat_x, mat_y)
+    def __init__(self, mat_x, mat_y):
+        self.op = self.decision_tree_learning(mat_x, mat_x, mat_y)
         self.kids = []
         # 1 or 0
         self.leaf = []
 
-    def decision_tree_learning(examples, attributes, binary_target):
-        if(is_same_binary(binary_target)):
+    # def initialize(self):
+    #     if(is_same_binary(binary_target)):
+    #         leaf = binary_target[0]
+    #         return leaf
+    #
+    #     elif(np.binary_target([])):
+    #         leaf =  majority_value(binary_target)
+    #         return leaf
+    #
+    #     else:
+    #         leaf = choose_best_decision_attribute(examples, attributes, binary_target)
+    #         return leaf
+
+    def decision_tree_learning(self, examples, attributes, binary_target):
+        if(self.is_same_binary(binary_target)):
             leaf = binary_target[0]
             return leaf
 
@@ -62,7 +77,8 @@ class Tree:
             leaf = choose_best_decision_attribute(examples, attributes, binary_target)
             return leaf
 
-    def is_same_binary(binary_target):
+
+    def is_same_binary(self, binary_target):
          first_value = binary_target[0]
          for i in binary_target:
              if binary_target[i] == first_value:
@@ -71,8 +87,74 @@ class Tree:
                  return False
          return True
 
-    def majority_value(binary_target):
+    def majority_value(self, binary_target):
         return sio.stats.mode(binary_target)
 
-    def choose_best_decision_attribute(examples, attributes, binary_target):
-        
+    def choose_best_decision_attribute(self, examples, attributes, binary_target):
+        counter_0 = 0
+        counter_1 = 0
+        print("binary target", binary_target)
+        for x in binary_target:
+            if x == 0:
+                counter_0 += 1
+            else:
+                counter_1 +=1
+
+        print(counter_0)
+        print(counter_1)
+        print(counter_0/len(binary_target))
+        initial_entropy = -(counter_0/len(binary_target))*np.log2(counter_0/len(binary_target))-(counter_1/len(binary_target))*np.log2(counter_1/len(binary_target))
+        print(initial_entropy)
+
+        largest_info_gain = 0
+        for idx in range(len(attributes)):
+            info_gain = self.get_info_gain(binary_target, examples, initial_entropy, idx)
+            if(largest_info_gain < info_gain):
+                largest_info_gain = info_gain
+
+        print("largest info gain")
+        print(largest_info_gain)
+
+    def get_info_gain(self, binary_target, examples, initial_entropy, column):
+        c_p = 0
+        c_n = 0
+        c_p_0_p = 0
+        c_n_0_n = 0
+        c_p_0_n = 0
+        c_n_0_p = 0
+
+        column = examples[:, column]
+
+        i=0
+        for r in column:
+            if r == 1:
+                c_p += 1
+                if binary_target[i] == 1:
+                    c_p_0_p += 1
+            if r == 0:
+                if binary_target[i] == 1:
+                    c_n_0_p += 1
+            i += 1
+
+        c_n = len(binary_target) - c_p
+        c_p_0_n = c_p - c_p_0_p
+
+        c_n_0_n = c_n - c_n_0_p
+
+        # print(c_n) # negative attribute examples
+        # print(c_p) # postive attribute examples
+        # print(c_p_0_n) # positive attribute, negative  examples
+        # print(c_n_0_n) # negative attribute, negative target
+        # print(c_p_0_p) # positive attibute, positive target
+        # print(c_n_0_p) # negative attribute, positive target
+
+        i1 = - (c_p_0_p /(c_p_0_p + c_p_0_n))*np.log2(c_p_0_p/(c_p_0_p + c_p_0_n)) - (c_p_0_n /(c_p_0_p + c_p_0_n))*np.log2(c_p_0_n/(c_p_0_p + c_p_0_n))
+        i0 = - (c_n_0_p /(c_n_0_p + c_n_0_n))*np.log2(c_n_0_p/(c_n_0_p + c_n_0_n)) - (c_n_0_n /(c_n_0_p + c_n_0_n))*np.log2(c_n_0_n/(c_n_0_p + c_n_0_n))
+
+        after_entropy = ((c_n)/len(binary_target))*i0 +  ((c_p)/len(binary_target))* i1
+
+        info_gain = initial_entropy - after_entropy
+        return info_gain
+
+a  = Tree(mat_x, mat_y)
+a.choose_best_decision_attribute(mat_x, attributes, mat_y)
