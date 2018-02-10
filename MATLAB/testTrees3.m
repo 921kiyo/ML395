f5603790c184edab8b057f71618569c51b7ba22a
Kiyohito Kunii <kk3317@edge32.doc.ic.ac.uk>
2018-02-10 18:13:49 +0000

    add randi to testTree2.m and update testTree3.m

--- MATLAB/testTrees3.m
+++ MATLAB/testTrees3.m
@@ -1,22 +1,22 @@
-function [ result ] = testTrees3(T, x2)
+function [ result ] = testTrees3(T, x2, targets)
 %TESTTREES3 Summary of this function goes here
-%   Detailed explanation goes here
-% classes = [0 1];
-% f_1_array = zeros(1,6);
-% 
-% for i = 1:6
-%    [overall_accuracy, accuracy, recall, precision, f_1] = evaluate_metrics(binary_predictions, binary_target, classes);
-%    
-
-% end
-
-
+% if no binary trees predict a class, we choose randomly
+% if one binary tree predicts a class, we select that tree
+% if >1 binary tree predicts a class, we select the tree with the highest
+% F1 score achieved during training (hardcoded)
 
 binary_predictions = zeros(size(x2,1),6);
 
 for i = 1:length(T)
     pred = prediction(T(i), x2);
-    binary_prediction(:,i) = pred;
+    binary_predictions(:,i) = pred;
+end
+
+for i = 1:6
+    classes = 1;
+    bin_targets = binary_targets(i, targets);
+    [overall_accuracy, accuracy, recall, precision, f_1] = evaluate_binary_metrics(binary_predictions(:,i), bin_targets, classes);
+    disp(f_1);
 end
 
 % TODO: Hardcode f scores. When the trees are tested, they will have
@@ -31,9 +31,6 @@ result = zeros(l,1);
 for i = 1:l
     % i.e no trees predict that the example has the emotion
     if(sum(binary_predictions(i,:)) == 0)
-        % Choose the common emotion from the training
-        % TODO rather than the maximum F1
-%         result(i) = max(f_1_array);
         result(i) = randi([1,6]);   
     elseif(sum(binary_predictions(i,:)) == 1)
         for j = 1:6
@@ -47,26 +44,28 @@ for i = 1:l
         % f_a_candidates
         predictions = {};
         f_1_candidates = {};
-        for j = 1:6
-            if binary_predictions(i,j) == 1
-                predictions = [predictions,j];
-                f_1_candidates = [f_1_candidates, f_1_array(j)];
-                % Store both predictions and F1 in [[pred1, F1_1], [pred2,
-                % F1_2] ...] 
+        for k = 1:6
+            if binary_predictions(i,k) == 1
+                predictions = [predictions,k];
+                f_1_candidates = [f_1_candidates, f_1_array(k)];
             end
         end
+        
+        if isa(cell2mat(f_1_candidates), 'double')
+            f_1_candidates_double = cell2mat(f_1_candidates);
+        end
+          
         num_pred = numel(predictions);
         max_f1 = 0;
         best_discriminant = 0;
-        for i = 1:num_pred;
-            if max_f1 < f_1_candidates(i)
-                best_discriminant = i;
-                max_f1 = f_1_candidates(i);
+        for m = 1:num_pred;
+            if max_f1 < f_1_candidates_double(m)
+                best_discriminant = predictions(m);
+                max_f1 = f_1_candidates_double(m);
             end
         end
-        result(i) = best_discriminant;
+        result(i) = cell2mat(best_discriminant);
     end
 end
 
 end
-
